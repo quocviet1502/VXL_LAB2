95e74653035308d62d04bd92e015fd6603d3eae9
Nguyen Quoc Viet <viet.nguyenvieta1k19@hcmut.edu.vn>
2023-10-31 15:39:31 +0700

    init_ex9

---
+++ STM32PROJECT/Core/Inc/software_timer.h
@@ -0,0 +1,21 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 31, 2023
+ *      Author: Quoc Viet
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+extern int timer1_flag;
+extern int timer2_flag;
+extern int timer3_flag;
+void setTimer0(int duration);
+void setTimer1(int duration);
+void setTimer2(int duration);
+void setTimer3(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
